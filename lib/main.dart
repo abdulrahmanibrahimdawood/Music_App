38141c86af1a9c4266e64c7d1063bb5aae428ad4
abdulrahmanibrahimdawood <abdoibrahim9111@gmail.com>
2024-10-01 00:37:38 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MusicApp());
+}
+
+class MusicApp extends StatelessWidget {
+  const MusicApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
